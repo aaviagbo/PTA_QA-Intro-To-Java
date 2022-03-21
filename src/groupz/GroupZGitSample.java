c34fb74d6e533bddbce7654c93527b02357ccda9
emileqae <emileesapa@gmail.com>
2022-03-21 18:47:00 -0500

    GroupZSample.java created and completed

---
+++ src/groupz/GroupZGitSample.java
@@ -0,0 +1,22 @@
+/**
+ * Project Descript: Create a sample project to demonstate git commands
+ * Authors: PTA MAnager, Milo
+ * Date: 3/21/2022
+ * */
+
+package groupz;
+
+public class GroupZGitSample {
+
+	public static void main(String[] args) {
+		String nameofSchool = "Pro-Tech Alliance";
+		String locationState = "Texas";
+		String country = "USA";
+		int zipCode = 12345;
+		
+		System.out.println("Arnold Ngamelue and Bet Sunrise and Classmates attend "+nameofSchool);
+		System.out.println("Sapherine and Nick are located in "+locationState+" "+country+
+				" "+zipCode);
+	}
+
+}
