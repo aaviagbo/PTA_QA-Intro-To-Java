f11bce1ed3f3a7158393228bc1cdb9bbba3cfe85
emileqae <emileesapa@gmail.com>
2022-03-20 19:12:51 -0500

    assignment1 completed on Assignment1.java

---
+++ src/groupb/Assignment1.java
@@ -0,0 +1,67 @@
+/**
+ * Names: William, Sapherine, Niki
+ * Date: 03/20/2022
+ * 
+ * Project:
+ * Get the names of governors in 10 States in the USA. 
+ * Include the gas prices of each state and the names of the state
+ * Print the result on each line
+ * */
+
+package groupb;
+
+public class Assignment1 {
+
+	public static void main(String[] args) {
+		
+				String State0 = "Alaska";
+				    String governorName0 = "Mike Dunleavy";
+				    String GasPrice0 = "4.70";
+				    
+				    String State1 ="Minnesota";
+				    String governorName1 = "Tim Walz";
+				    String GasPrice1 = "3.99";
+				    
+				    String State2 ="Maryland";
+				    String governorName2 = "Larry hogan";
+				    String GasPrice2 = "3.95";
+				    
+				    String State3 = "Texas";
+				    String governorName3 = "Greg Abbott";
+				    String GasPrice3 = "3.96";
+				    
+				    String State4 = "Utah";
+				    String governorName4 = "Spency Cox";
+				    String GasPrice4 = "4.34";
+				    
+				    String State5 = "Ohio";
+				    String governorName5 = "Mike Dewine";
+				    String GasPrice5 = "4.04";
+				    
+				    String State6 = "North Dakota";
+				    String governorName6 = "Doug Burgum";
+				    String GasPrice6 = "3.88";
+				    
+				    String State7 = "New York";
+				    String governorName7 = "Kathy Hochul";
+				    String GasPrice7 = "4.37";
+				    
+				    String State8 = "New Jersey";
+				    String governorName8 = "Phil Murphy";
+				    String GasPrice8 = "4.23";
+
+
+	
+				  System.out.println(State1+" "+ governorName1 +" "+ GasPrice1);
+				  System.out.println(State2+" "+ governorName2 +" "+ GasPrice2);
+				  System.out.println(State3+" "+ governorName3 +" "+ GasPrice3);
+				  System.out.println(State4+" "+ governorName4 +" "+ GasPrice4);
+				  System.out.println(State5+" "+ governorName5 +" "+ GasPrice5);
+				  System.out.println(State6+" "+ governorName6 +" "+ GasPrice6);
+				  System.out.println(State7+" "+ governorName7 +" "+ GasPrice7);
+				  System.out.println(State8+" "+ governorName8 +" "+ GasPrice8);
+				  System.out.println(State0+" "+ governorName0 +" "+ GasPrice0);
+	}
+	
+
+}
