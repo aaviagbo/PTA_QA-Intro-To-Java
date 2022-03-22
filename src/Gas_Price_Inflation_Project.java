729873e65afce29efa5d27b8944fe116946d6aed
PROFOBASSE <obaserudolf2@gmail.com>
2022-03-22 17:41:05 -0500

    updates

---
+++ src/Gas_Price_Inflation_Project.java
@@ -0,0 +1,101 @@
+
+
+public class Gas_Price_Inflation_Project {
+
+	public static void main(String[] args) {
+		
+		String state1= "Texas";
+		String Govname1 = "Greg Abbott";
+        Double price1=3.08;
+        
+        String state2 = "Maryland";
+        String Govname2 ="Larry Hogan";
+        Double price2=4.03;
+        
+        String state3 = "NewYork";
+        String Govname3 ="KathyHorcul";
+        Double price3=5.00;
+        
+        String state4 ="NewJesey";
+        String Govname4 ="PhilMurphy";
+        Double price4 = 2.5;
+        
+        String state5 ="Califonia";
+        String Govname5 ="RonDesantis";
+        Double price5 =5.99;
+        
+        String state6 ="Florida";
+        String Govname6 ="MichelleLujan";
+        Double price6 =4.89;
+        
+        String state7 ="NewMexico";
+        String Govname7 ="JaredPolis";
+        Double price7 =3.55;
+        
+        String state8 ="Colorado";
+        String Govname8 ="JaredPolis";
+        Double price8 =6.00;
+	
+        String state9 ="Nevada";
+        String Govname9 ="Shantal";
+        Double price9 =5.25;
+        
+        String state10 ="NewMexico";
+        String Govname10 ="MichelleLujan";
+        Double price10 =4.13;
+	
+	
+    System.out.println("Name of State = " + state1);
+	System.out.println("The name of gov. is " + Govname1);
+	System.out.println("Price of gas = $" + price1+"\n");
+	
+    System.out.println("Name of State = " + state2);
+	System.out.println("The name of gov. is " + Govname2);
+	System.out.println("Price of gas = $" + price2+"\n");
+	
+	System.out.println("Name of State = " + state1);
+	System.out.println("The name of gov. is " + Govname1);
+	System.out.println("Price of gas = $" + price1);
+	
+    System.out.println("Name of State = " + state3);
+	System.out.println("The name of gov. is " + Govname3);
+	System.out.println("Price of gas = $" + price3);
+	
+	
+	System.out.println("Name of State = " + state4);
+	System.out.println("The name of gov. is " + Govname3);
+	System.out.println("Price of gas = $" + price4);
+	
+	System.out.println("Name of State = " + state5);
+	System.out.println("The name of gov. is " + Govname5);
+	System.out.println("Price of gas = $" + price5);
+	
+	
+	System.out.println("Name of State = " + state6);
+	System.out.println("The name of gov. is " + Govname6);
+	System.out.println("Price of gas = $" + price6);
+	
+
+	System.out.println("Name of State = " + state7);
+	System.out.println("The name of gov. is " + Govname7);
+	System.out.println("Price of gas = $" + price7);
+	
+	System.out.println("Name of State = " + state8);
+	System.out.println("The name of gov. is " + Govname8);
+	System.out.println("Price of gas = $" + price8);
+	
+	System.out.println("Name of State = " + state9);
+	System.out.println("The name of gov. is " + Govname9);
+	System.out.println("Price of gas = $" + price9);
+	
+	
+	System.out.println("Name of State = " + state10);
+	System.out.println("The name of gov. is " + Govname10);
+	System.out.println("Price of gas = $" + price10);
+	
+	
+	System.out.println("The total price of gas in all ten States is =$" +(price1+price2+price3+price4+price5+price6+price7+price8+price9+price10));
+
+	}
+
+}
