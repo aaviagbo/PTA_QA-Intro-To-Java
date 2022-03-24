b3535c4a5f547310c9bcd3347baaf904e5af4b74
PROFOBASSE <obaserudolf2@gmail.com>
2022-03-24 18:35:27 -0500

    Fixed groupb assignment

--- src/groupb/Assignment1.java
+++ src/groupb/Assignment1.java
@@ -17,51 +17,51 @@ public class Assignment1 {
 		
 				String State0 = "Alaska";
 				    String governorName0 = "Mike Dunleavy";
-				    String GasPrice0 = "4.70";
+				    double  GasPrice0 = 4.70;
 				    
 				    String State1 ="Minnesota";
 				    String governorName1 = "Tim Walz";
-				    String GasPrice1 = "3.99";
+				    double  GasPrice1 = 3.99;
 				    
 				    String State2 ="Maryland";
 				    String governorName2 = "Larry hogan";
-				    String GasPrice2 = "3.95";
+				    double  GasPrice2 = 3.95;
 				    
 				    String State3 = "Texas";
 				    String governorName3 = "Greg Abbott";
-				    String GasPrice3 = "3.96";
+				    double  GasPrice3 = 3.96;
 				    
 				    String State4 = "Utah";
 				    String governorName4 = "Spency Cox";
-				    String GasPrice4 = "4.34";
+				    double  GasPrice4 = 4.34;
 				    
 				    String State5 = "Ohio";
 				    String governorName5 = "Mike Dewine";
-				    String GasPrice5 = "4.04";
+				    double GasPrice5 = 4.04;
 				    
 				    String State6 = "North Dakota";
 				    String governorName6 = "Doug Burgum";
-				    String GasPrice6 = "3.88";
+				    double  GasPrice6 = 3.88;
 				    
 				    String State7 = "New York";
 				    String governorName7 = "Kathy Hochul";
-				    String GasPrice7 = "4.37";
+				    double  GasPrice7 = 4.37;
 				    
-				    String State8 = "New Jersey";
-				    String governorName8 = "Phil Murphy";
-				    String GasPrice8 = "4.23";
+				   String State8 = "New Jersey";
+				   String governorName8 = "Phil Murphy";
+				   double GasPrice8 = 4.23;
 
 
 	
-				  System.out.println(State1+" "+ governorName1 +" "+ GasPrice1);
-				  System.out.println(State2+" "+ governorName2 +" "+ GasPrice2);
-				  System.out.println(State3+" "+ governorName3 +" "+ GasPrice3);
-				  System.out.println(State4+" "+ governorName4 +" "+ GasPrice4);
-				  System.out.println(State5+" "+ governorName5 +" "+ GasPrice5);
-				  System.out.println(State6+" "+ governorName6 +" "+ GasPrice6);
-				  System.out.println(State7+" "+ governorName7 +" "+ GasPrice7);
-				  System.out.println(State8+" "+ governorName8 +" "+ GasPrice8);
-				  System.out.println(State0+" "+ governorName0 +" "+ GasPrice0);
+				  System.out.println(State1+", "+ governorName1 +", gas price is $"+ GasPrice1+"\n");
+				  System.out.println(State2+", "+ governorName2 +", gas price is $"+ GasPrice2+"\n");
+				  System.out.println(State3+", "+ governorName3 +", gas price is $"+ GasPrice3+"\n");
+				  System.out.println(State4+", "+ governorName4 +", gas price is $"+ GasPrice4+"\n");
+				  System.out.println(State5+", "+ governorName5 +", gas price is $"+ GasPrice5+"\n");
+				  System.out.println(State6+", "+ governorName6 +", gas price is $"+ GasPrice6+"\n");
+				  System.out.println(State7+", "+ governorName7 +", gas price is $"+ GasPrice7+"\n");
+				  System.out.println(State8+", "+ governorName8 +", gas price is $"+ GasPrice8+"\n");
+				  System.out.println(State0+", "+ governorName0 +", gas price is $"+ GasPrice0+"\n");
 	}
 	
 
