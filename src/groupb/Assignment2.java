f2879c3377a37a4779c8cb42bf30248337807049
SAPH500 <101662056+SAPH500@users.noreply.github.com>
2022-03-23 21:54:08 -0400

    assingment 2 completed with nik saph and kathy

---
+++ src/groupb/Assignment2.java
@@ -0,0 +1,16 @@
+package groupb;
+
+public class Assignment2 {
+
+	public static void main(String[] args) {
+		
+		String name = "Milo";
+		
+		System.out.println("Nick is a pa");
+		System.out.println("Kay is a girl");
+		System.out.println("Sapherine is a girl");
+		System.out.println(name);
+
+	}
+
+}
