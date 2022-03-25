4c56d24112ce74001d715a0e9bc00fe3161638a7
SAPH500 <101662056+SAPH500@users.noreply.github.com>
2022-03-24 20:13:11 -0400

    PTAREVISION2022 Assignment completed

---
+++ src/PTAREVISION2022.java
@@ -0,0 +1,27 @@
+/**
+ * NAME OF 3 CELLS,PRICES OF CELLS PHONES,TOTAL OF ALL 3 CELL PHONES
+ */
+public class PTAREVISION2022 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          
+		String NamePhone1 = "iphone";
+		String NamePhone2 = "samsung";
+		String NamePhone3 = "nokia";
+		
+		double PriceOfPhone1 = 1200;
+		double PriceOfPhone2 = 1000;
+		double PriceOfPhone3 = 800;
+		
+		System.out.println (NamePhone1 + " Price is $" + PriceOfPhone1);
+		System.out.println (NamePhone2 + " Price is $" + PriceOfPhone2);
+		System.out.println (NamePhone3 + " Price is $" + PriceOfPhone3);
+		 
+		System.out.println ("Total Price Of All 3 Phones are " + ( PriceOfPhone1 + PriceOfPhone2 + PriceOfPhone3));
+
+	
+				
+	}
+
+}
