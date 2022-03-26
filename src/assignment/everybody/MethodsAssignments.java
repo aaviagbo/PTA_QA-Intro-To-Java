d2f9c45039c146bb36565a5a253ef6d6033be2ad
emileqae <emileesapa@gmail.com>
2022-03-25 20:18:47 -0500

    assingment method assigned to all class

---
+++ src/assignment/everybody/MethodsAssignments.java
@@ -0,0 +1,16 @@
+package assignment.everybody;
+/**
+ * Create 5 methods of anything 
+ * those methods should all be called in the main method
+ * do not type your values....make use of Global and local variables
+ * USE this as a learning tool:https://www.w3schools.com/java/java_variables.asp
+ * 
+ * */
+public class MethodsAssignments {
+
+	public static void main(String[] args) {
+
+
+	}
+
+}
